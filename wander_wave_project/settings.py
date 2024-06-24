9f359ae0fb5769233521af96aadb53c23a5941f0
rusip <rusipbox@gmail.com>
2024-06-24 15:30:42 +0300

    added path to the media

--- wander_wave_project/settings.py
+++ wander_wave_project/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = "/files/media"
+
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
