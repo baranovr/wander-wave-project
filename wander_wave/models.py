1ed8e5d0c2b490fa39585adf062c031772783eaa
rusip <rusipbox@gmail.com>
2024-07-25 13:07:28 +0300

    name filed in Location can be blunk and null

--- wander_wave/models.py
+++ wander_wave/models.py
@@ -106,7 +106,9 @@ class SubscriptionNotification(models.Model):
 class Location(models.Model):
     country = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(
+        max_length=255, unique=True, blank=True, null=True
+    )
 
     def save(self, *args, **kwargs):
         self.name = f"{self.country}, {self.city}"
