632d1e0c57f2363652279d4256aead35b42e9096
rusip <rusipbox@gmail.com>
2024-08-05 12:34:35 +0300

    changed lifetime of JWTs access/refresh tokens

--- backend/wander_wave_project/settings.py
+++ backend/wander_wave_project/settings.py
@@ -185,8 +185,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=28),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
