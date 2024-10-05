43bcc8391aa226f18c28ecac295a4cfdbca054f0
Ruslan <rusipbox@gmail.com>
2024-10-05 14:22:09 +0300

    deleted all backend elements, deleted frontend folder

--- user/custom_token/token_view.py
+++
@@ -1,7 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-from user.custom_token.token_serializers import CustomTokenObtainPairSerializer
-
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
