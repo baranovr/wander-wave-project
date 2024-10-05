b714a3b4053af3dd5a65398c17f0fc935616c8fe
Ruslan <rusipbox@gmail.com>
2024-10-05 22:54:34 +0300

    added STATIC_ROOT

--- wander_wave_project/settings.py
+++ wander_wave_project/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
@@ -146,10 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_ROOT = "staticfiles/"
 
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
