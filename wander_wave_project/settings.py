ca0e3e7e4a868893dc09ffabf53000180ce9f9fd
Ruslan <rusipbox@gmail.com>
2024-10-06 03:39:51 +0300

    set AWS settings

--- wander_wave_project/settings.py
+++ wander_wave_project/settings.py
@@ -149,8 +149,17 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = "staticfiles/"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+
+# AWS settings
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
