a04aeeeea1881a9f2b1d969635f9c58cce5b4f34
baranovr <rusipbox@gmail.com>
2024-10-08 17:06:53 +0300

    added wander-wave-service to CORS_ALLOWED_ORIGINS

--- wander_wave_project/settings.py
+++ wander_wave_project/settings.py
@@ -215,6 +215,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://wander-wave-service.onrender.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
